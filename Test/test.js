caf9bd1a51966f38c5d61c55f265d1e89af31924
SerG <seriytitarenko@gmail.com>
2019-10-28 20:24:24 +0200

    first commit

---
+++ Test/test.js
@@ -0,0 +1,10 @@
+var msg = 'INFO'
+var notification = document.querySelector('.notification');
+
+var valid = false;
+
+if(msg === 'valid') {
+    valid = true;
+} else {
+    valid = false;
+};
---
+++ script.js
@@ -0,0 +1,28 @@
+var cellName = 'CELL 1';
+var num = .2;
+var str = 'abc';
+var bool = true;
+var n = null;
+var un;
+
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(cellName);
+console.log(typeof cellName);
+
+
+var num = 5;
+var otherNum = num++;
+var oneMoreNum = ++num;
+
+console.log(num, otherNum, oneMoreNum);
+
+
+var msg = confirm('What do you mean?', 'Okay');
+console.log(msg);
